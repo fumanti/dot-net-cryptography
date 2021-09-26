a6315cdf6f81b5cc3ccaa8d6454ed5af8cd89c17
Stefano Fumanti <fumanti@hotmail.com>
2021-09-26 20:46:33 +0200

    Add salt to Hashing

--- Hashing.cs
+++ Hashing.cs
@@ -1,23 +1,32 @@
+using System;
 using System.Security.Cryptography;
 
 namespace secure_applications_cryptography
 {
     public static class Hashing
     {
-        public static byte[] ComputeHash_MD5(byte[] toBeHashed)
+        public static byte[] ComputeHash_MD5(byte[] toBeHashed, byte[] salt = null)
         {
             using (var md5 = MD5.Create())
             {
-                return md5.ComputeHash(toBeHashed);
+                return md5.ComputeHash(salt != null ? Combine(toBeHashed, salt) : toBeHashed);
             }
         }
 
-        public static byte[] ComputeHash_SHA512(byte[] toBeHashed)
+        public static byte[] ComputeHash_SHA512(byte[] toBeHashed, byte[] salt = null)
         {
             using (var sha512 = SHA512.Create())
             {
-                return sha512.ComputeHash(toBeHashed);
+                return sha512.ComputeHash(salt != null ? Combine(toBeHashed, salt) : toBeHashed);
             }
+        }
+
+        public static byte[] Combine(byte[] password, byte[] salt)
+        {
+            var ret = new byte[password.Length + salt.Length];
+            Buffer.BlockCopy(password, 0, ret, 0, password.Length);
+            Buffer.BlockCopy(salt, 0, ret, password.Length, salt.Length);
+            return ret;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace secure_applications_cryptography
         static void Main(string[] args)
         {
             // Execute(RandomNumberGenerator);
-            // Execute(HashData);
+            Execute(HashData);
             //Execute(HMAC);
-            Execute(Pbkdf2);
+            //Execute(Pbkdf2);
         }
 
         static void Execute(Action action)
@@ -45,16 +45,26 @@ namespace secure_applications_cryptography
             Console.WriteLine($"Original message 1: {originalMessage}");
             Console.WriteLine($"Original message 2: {originalMessage2}");
             Console.WriteLine($"");
-            
-            var md5_1 = Hashing.ComputeHash_MD5(System.Text.Encoding.UTF8.GetBytes(originalMessage));
-            var md5_2 = Hashing.ComputeHash_MD5(System.Text.Encoding.UTF8.GetBytes(originalMessage2));
+
+            byte[] salt = null;
+            Console.WriteLine($"Do you want to add salt ? (y/n)");
+            var inputYesNo = Console.ReadKey();
+            if (inputYesNo.KeyChar == 'y')
+			   {
+               salt = Random.GenerateRandomNumber(32);
+               Console.WriteLine($"");
+               Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+            }
+
+            var md5_1 = Hashing.ComputeHash_MD5(System.Text.Encoding.UTF8.GetBytes(originalMessage), salt);
+            var md5_2 = Hashing.ComputeHash_MD5(System.Text.Encoding.UTF8.GetBytes(originalMessage2), salt);
 
             Console.WriteLine($"Hash MD5 1: {Convert.ToBase64String(md5_1)}");
             Console.WriteLine($"Hash MD5 2: {Convert.ToBase64String(md5_2)}");
             Console.WriteLine($"");
 
-            var sha512_1 = Hashing.ComputeHash_SHA512(System.Text.Encoding.UTF8.GetBytes(originalMessage));
-            var sha512_2 = Hashing.ComputeHash_SHA512(System.Text.Encoding.UTF8.GetBytes(originalMessage2));
+            var sha512_1 = Hashing.ComputeHash_SHA512(System.Text.Encoding.UTF8.GetBytes(originalMessage), salt);
+            var sha512_2 = Hashing.ComputeHash_SHA512(System.Text.Encoding.UTF8.GetBytes(originalMessage2), salt);
 
             Console.WriteLine($"Hash SHA512 1: {Convert.ToBase64String(sha512_1)}");
             Console.WriteLine($"Hash SHA512 2: {Convert.ToBase64String(sha512_2)}");
@@ -87,7 +97,7 @@ namespace secure_applications_cryptography
 
       static void Pbkdf2()
       {
-         //const string password = "V3r7K0mP1&xP4$$w07d";
+         //const string password = "V3r7K0mP1&xP4$$w0?d";
          const string password = "the password";
 
          Console.Clear();
